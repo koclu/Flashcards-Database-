877f4ece8308138ac32f2626d8a50de2039da534
KadrEfe <efe.akadr@gmail.com>
2021-12-15 23:15:04 +0100

    Fixed Stats

--- statistics.py
+++ statistics.py
@@ -15,13 +15,14 @@ from PyQt5 import QtCore, QtWidgets, uic
 from user import Users
 import menu
 import psycopg2
+import db
 
 class Statistics_Window(QtWidgets.QMainWindow):
      def __init__(self, user):
         self.user = user
         super(Statistics_Window, self).__init__()
         uic.loadUi('Ui/statistics.ui', self)
-
+        
         self.quitbutton.clicked.connect(self.backk)
         self.show()
         conn = psycopg2.connect(
@@ -30,14 +31,20 @@ class Statistics_Window(QtWidgets.QMainWindow):
         user='postgres',
         password="Alvo1.")
         
-        cur = conn.cursor()
+        db.cur = conn.cursor()
+        query3="""SELECT count(user_name)
+FROM public."Users";"""
+        info3 = db.cur.execute(query3)
+        info3 = db.cur.fetchall()
+        
+        self.mainmenu_label.setText(("Numbers Of Players :   {} ".format(info3[0][0])))
         
         query=f"""select u.user_name,MAX(s.completed_level),ROW_NUMBER() OVER(order by MAX(s.completed_level) desc) as row_number from public."Users"  as u
 join public."Statistics" as s on s.user_name = u.user_name
 group by u.user_name"""
-        info = cur.execute(query)
-        info = cur.fetchall()
-        print(info)
+        info = db.cur.execute(query)
+        info = db.cur.fetchall()
+        
         #  info da kullanicilar (isim,maxlevel)
         
         for i in range(5):
@@ -45,8 +52,8 @@ group by u.user_name"""
                 query2="""select user_name , AVG(100*(knows/attemps::float))  as "ka" from public."Statistics"  
         where user_name ='%s'
         group by user_name"""%(info[i][0])
-                info2 = cur.execute(query2)
-                info2 = cur.fetchall()
+                info2 = db.cur.execute(query2)
+                info2 = db.cur.fetchall()
                 #info2 nin icine avaragelar (isim,oran) seklinde gelecek
                 if i==0:
                    self.progressBar_menu_1.setProperty("value", (info2[0][1]))
@@ -58,7 +65,6 @@ group by u.user_name"""
                    self.progressBar_menu_4.setProperty("value", (info2[0][1]))
                 if i==4:
                    self.progressBar_menu_5.setProperty("value", (info2[0][1]))
-                print(info2[0][1])
                 i+=1    
         
 
@@ -83,12 +89,7 @@ group by u.user_name"""
 
 
 #toplam kullanici
-        query3="""SELECT count(user_name)
-FROM public."Users";"""
-        info3 = cur.execute(query3)
-        info3 = cur.fetchall()
-        print(info3[0][0])
-        self.mainmenu_label.setText(("Numbers Of Players :   {} ".format(info3[0][0])))
+        
 
    
        
